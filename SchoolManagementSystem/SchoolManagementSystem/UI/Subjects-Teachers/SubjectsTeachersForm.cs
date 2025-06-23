768f20ef5e10880f9eecfb43afdaecaa618f4e9b
Abishan <abishanjeyarajah@gmail.com>
2025-06-23 12:20:24 +0530

    add forms to gradessubjects andsubjectsteacher

---
+++ SchoolManagementSystem/SchoolManagementSystem/UI/Subjects-Teachers/SubjectsTeachersForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolManagementSystem.UI.Subjects_Teachers
+{
+    public partial class SubjectsTeachersForm : Form
+    {
+        public SubjectsTeachersForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
